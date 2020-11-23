892d259a574be9bab2791e72adca08871cfaa294
Alane Parisot <27372097+alaneparisot@users.noreply.github.com>
2020-11-23 07:18:24 +0100

    Add Compression Middleware

--- src/loaders/middlewares.ts
+++ src/loaders/middlewares.ts
@@ -1,8 +1,10 @@
+import compression from 'compression'
 import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
 
 export function init(app: express.Application): void {
+  app.use(compression())
   app.use(helmet())
   app.use(cors())
   app.use(express.json())
