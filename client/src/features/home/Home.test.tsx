09e49f7dacc84e54af2aea16ebde9b849fbe4152
Alane Parisot <27372097+alaneparisot@users.noreply.github.com>
2021-05-13 12:12:35 +0200

    Update Home unit tests

--- client/src/features/home/Home.test.tsx
+++ client/src/features/home/Home.test.tsx
@@ -2,22 +2,22 @@ import { render, screen } from '../../utils/test-utils'
 import { Home } from './Home'
 
 describe('Home', () => {
-  it('should display welcome message without username if user is not connected', () => {
+  it('should display simple welcome message when user not connected', () => {
     // Act
     render(<Home />)
 
     // Assert
-    expect(screen.getByText(/i18n-welcome/)).toBeInTheDocument()
+    expect(screen.getByText('i18n-welcome')).toBeInTheDocument()
   })
 
-  it('should display welcome message with username if user is connected', () => {
+  it('should display welcome message with username when user connected', () => {
     // Arrange
     const initialState = {
       user: {
         currentUser: {
-          _id: 'user-id-1',
-          username: 'user-name-1',
-          token: 'user-token-1',
+          _id: '',
+          username: 'johndoe',
+          token: '',
         },
       },
     }
@@ -26,7 +26,6 @@ describe('Home', () => {
     render(<Home />, { preloadedState: initialState })
 
     // Assert
-    expect(screen.getByText(/i18n-welcomeName/)).toBeInTheDocument()
-    expect(screen.getByText(/user-name-1/)).toBeInTheDocument()
+    expect(screen.getByText(/^i18n-welcomeName.+johndoe/)).toBeInTheDocument()
   })
 })
