e1ebd22b0c7ef3ea3fa6406958162e4993fb4bfe
Alane Parisot <27372097+alaneparisot@users.noreply.github.com>
2021-05-17 21:19:49 +0200

    Improve Auth middlewares tests

--- src/__tests__/components/auth/auth.middleware.test.ts
+++ src/__tests__/components/auth/auth.middleware.test.ts
@@ -3,12 +3,23 @@ import { hasCredentials } from '../../../components/auth/auth.middlewares'
 
 describe('Auth Middlewares', () => {
   describe('hasCredentials', () => {
-    it('should return a 400 status code if username is missing', () => {
+    type TestHasCredentialsParams = {
+      reqBody: {
+        username?: string
+        password?: string
+      }
+      shouldCallNext: boolean
+      shouldSend400: boolean
+    }
+
+    const testHasCredentials = ({
+      reqBody,
+      shouldCallNext,
+      shouldSend400,
+    }: TestHasCredentialsParams) => {
       // Arrange
       const mockReq = {
-        body: {
-          password: '1234567',
-        },
+        body: reqBody,
       } as Request
 
       const mockRes = {
@@ -21,53 +32,45 @@ describe('Auth Middlewares', () => {
       hasCredentials(mockReq, mockRes, mockNext)
 
       // Assert
-      expect(mockRes.sendStatus).toHaveBeenCalledWith(400)
-      expect(mockNext).not.toHaveBeenCalled()
+      shouldSend400
+        ? expect(mockRes.sendStatus).toHaveBeenCalledWith(400)
+        : expect(mockRes.sendStatus).not.toHaveBeenCalledWith(400)
+
+      shouldCallNext
+        ? expect(mockNext).toHaveBeenCalled()
+        : expect(mockNext).not.toHaveBeenCalled()
+    }
+
+    it('should send a 400 status code if all credentials are missing', () => {
+      testHasCredentials({
+        reqBody: {},
+        shouldSend400: true,
+        shouldCallNext: false,
+      })
     })
 
-    it('should return a 400 status code if password is missing', () => {
-      // Arrange
-      const mockReq = {
-        body: {
-          username: 'johndoe',
-        },
-      } as Request
-
-      const mockRes = {
-        sendStatus: jest.fn(),
-      } as unknown as Response
-
-      const mockNext = jest.fn() as NextFunction
-
-      // Act
-      hasCredentials(mockReq, mockRes, mockNext)
-
-      // Assert
-      expect(mockRes.sendStatus).toHaveBeenCalledWith(400)
-      expect(mockNext).not.toHaveBeenCalled()
+    it('should send a 400 status code if username is missing', () => {
+      testHasCredentials({
+        reqBody: { password: '1234567' },
+        shouldSend400: true,
+        shouldCallNext: false,
+      })
     })
 
-    it('should call the next function if both credentials are filled', () => {
-      // Arrange
-      const mockReq = {
-        body: {
-          username: 'johndoe',
-          password: '1234567',
-        },
-      } as Request
+    it('should send a 400 status code if password is missing', () => {
+      testHasCredentials({
+        reqBody: { username: 'johndoe' },
+        shouldSend400: true,
+        shouldCallNext: false,
+      })
+    })
 
-      const mockRes = {
-        sendStatus: jest.fn(),
-      } as unknown as Response
-
-      const mockNext = jest.fn() as NextFunction
-
-      // Act
-      hasCredentials(mockReq, mockRes, mockNext)
-
-      // Assert
-      expect(mockRes.sendStatus).not.toHaveBeenCalledWith(400)
-      expect(mockNext).toHaveBeenCalled()
+    it('should call the next function if all credentials are filled', () => {
+      testHasCredentials({
+        reqBody: { username: 'johndoe', password: '1234567' },
+        shouldSend400: false,
+        shouldCallNext: true,
+      })
     })
   })
 })
